7c3c1b55b3bcd8bfbb2b620f1815af403ed886ce
Joaquin Beltran <contact@joaquinmx.com>
2025-12-06 15:25:15 -0600

    update dependencies

--- toktik/android/app/src/main/kotlin/com/example/toktik/MainActivity.kt
+++ toktik/android/app/src/main/kotlin/com/example/toktik/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.toktik
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- toktik/lib/domain/datasources/video_posts_datasource.dart
+++ toktik/lib/domain/datasources/video_posts_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:toktik/domain/entities/video_post.dart';
 
-abstract class VideoPostDatasource {
+mixin VideoPostDatasource {
   Future<List<VideoPost>> getFavoriteVideosByUser(String userID);
   Future<List<VideoPost>> getTrendingVideosByPage(int page);
 }
